88823555ef16ac5484bd0e994844807660478b26
Zilll <Paroll_2%5%>
2022-08-07 19:45:43 +0300

    -typo

--- src/main/java/api/pet/post/BodyPost.java
+++ src/main/java/api/pet/post/BodyPost.java
@@ -6,22 +6,22 @@ import org.json.JSONObject;
 public class BodyPost {
 
     public String bodyCreatePet(Integer id, Integer categoryId, String categoryName, String name, String photoUrls, Integer tagsId, String tagsName, String status){
-        JSONArray jsonArrtags = new JSONArray();
-        JSONObject jsonObjcategory = new JSONObject();
-        jsonObjcategory.put("id",categoryId);
-        jsonObjcategory.put("name",categoryName);
+        JSONArray jsonArrTags = new JSONArray();
+        JSONObject jsonObjCategory = new JSONObject();
+        jsonObjCategory.put("id",categoryId);
+        jsonObjCategory.put("name",categoryName);
         JSONObject jsonObjtags = new JSONObject();
         JSONArray jsonObjphotoUrls = new JSONArray();
         jsonObjphotoUrls.put(photoUrls);
         jsonObjtags.put("id",tagsId);
         jsonObjtags.put("name",tagsName);
-        jsonArrtags.put(jsonObjtags);
+        jsonArrTags.put(jsonObjtags);
         JSONObject jsonObj = new JSONObject();
         jsonObj.put("id",id);
-        jsonObj.put("category",jsonObjcategory);
+        jsonObj.put("category",jsonObjCategory);
         jsonObj.put("name",name);
         jsonObj.put("photoUrls",jsonObjphotoUrls);
-        jsonObj.put("tags",jsonArrtags);
+        jsonObj.put("tags",jsonArrTags);
         jsonObj.put("status",status);
         return jsonObj.toString();
     }
