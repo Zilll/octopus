56c5bd60502c926ec31e3bd0ae05f77d4d83de87
Zilll <Paroll_2%5%>
2022-08-15 08:09:27 +0300

    - add key title

---
+++ src/main/java/api/RequestKeys.java
@@ -0,0 +1,15 @@
+package api;
+
+public class RequestKeys {
+    public static String ID= "id";
+    public static String NAME= "name";
+    public static String CATEGORY= "category";
+    public static String PHOTO_URLS= "photoUrls";
+    public static String TAGS= "tags";
+    public static String TAGS_ID= "tagsId";
+    public static String STATUS= "status";
+    public static String PET_ID= "petId";
+    public static String QUANTITY= "quantity";
+    public static String SHIP_DATE= "shipDate";
+    public static String COMPLETE= "complete";
+}
